2fb3d2126aa0aef367abef78cbedb0ca7ab98977
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-05-14 11:24:51 +0200

    move course component to seperate module, step 10

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,4 @@
-const Header = ({ name }) => <h2>{name}</h2>
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-)
-
-const Total = ({ total }) => <strong>total of {total} exercises</strong>
-
-const Course = ({ course }) => {
-  const total = course.parts.reduce((total, part) => total + part.exercises, 0)
-
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total total={total} />
-    </div>
-  )
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { render } from 'react-dom'
-import './index.css'
-import App from './App'
+import 'styles/index.css'
+import App from 'components/App'
 
 render(<App />, document.getElementById('root'))
