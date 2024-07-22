b7c3adce8de36ec701aa374ceccf23b134f019db
Marek Zelinka <mzelinka17@gmail.com>
2024-07-22 13:07:35 +0200

    solve ex2.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,57 @@
 function App() {
-  let course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      { id: 1, name: 'Fundamentals of React', exercises: 10 },
-      { id: 2, name: 'Using props to pass data', exercises: 7 },
-      { id: 3, name: 'State of a component', exercises: 14 },
-    ],
-  }
+  let courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ]
 
-  return <Course course={course} />
+  return (
+    <>
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </>
+  )
 }
 
 export default App
